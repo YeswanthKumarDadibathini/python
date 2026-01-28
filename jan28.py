4898dcc68292dabd4fb366c887fe8fa4b73e6995
YeswanthKumarDadibathini <dyeswanth1005@gmail.com>
2026-01-28 15:36:38 +0530

    inheritance class

--- jan28.py
+++ jan28.py
@@ -21,32 +21,70 @@
 # z=list(map(lambda x:x**2,list1))
 # print(z)
 
-class bank:
-    def __init__(self,bankACC,branch,withdraw,balance):
-        self.bankACC=bankACC
-        self.branch=branch
-        self.withdraw=withdraw
-        self.__balance=balance
-    def bankdet(self):
-        print(f"The bank account number {self.bankACC} and branch name {self.branch} amount to withdraw {self.withdraw} the remainind balance {self.__balance} ")
+# class bank:
+#     def __init__(self,bankACC,branch,withdraw,balance):
+#         self.bankACC=bankACC
+#         self.branch=branch
+#         self.withdraw=withdraw
+#         self.__balance=balance
+#     def bankdet(self):
+#         print(f"The bank account number {self.bankACC} and branch name {self.branch} amount to withdraw {self.withdraw} the remainind balance {self.__balance} ")
 
-    def get_balance(self):
-        return self.__balance 
+#     def get_balance(self):
+#         return self.__balance 
 
-details=bank(123456789,"Kadapa",10000,90000)
-details.bankdet()
+# details=bank(123456789,"Kadapa",10000,90000)
+# details.bankdet()
 
-class cars:
-    def __init__(self,car_model,owner_name,branch,carplate):
-        self.car_model=car_model
-        self.owner_name=owner_name
-        self.branch=branch
-        self.__carplate=carplate
-    def car_del(self):
-        print(f"model of the car {self.car_model} owners name {self.owner_name} location of the branch {self.branch} and car plate {self.__carplate} ")
+# class cars:
+#     def __init__(self,car_model,owner_name,branch,carplate):
+#         self.car_model=car_model
+#         self.owner_name=owner_name
+#         self.branch=branch
+#         self.__carplate=carplate
+#     def car_del(self):
+#         print(f"model of the car {self.car_model} owners name {self.owner_name} location of the branch {self.branch} and car plate {self.__carplate} ")
     
-    def get_plate(self):
-        return self.__carplate
+#     def get_plate(self):
+#         return self.__carplate
 
-detailss=cars('BMW','yeshu','kadapa',1)
-detailss.car_del()
+# detailss=cars('BMW','yeshu','kadapa',1)
+# detailss.car_del()
+
+# class student:
+#     def __init__(self,name,grade,percentage):
+#         self.name=name
+#         self.grade=grade
+#         self.percentage=percentage
+#     def student_call(self):
+#         print(f"Name of the student {self.name} grade {self.grade} percentage {self.percentage} ")
+
+# d=student("Yeshu","Btech",7.2)
+
+# class update(student):
+#     def __init__(self,name,grade,percentage,branch):
+#         super().__init__(name,grade,percentage)
+#         self.branch=branch
+    
+#     def update_call(self):
+#         print(f"Name of the student {self.name} grade {self.grade} percentage {self.percentage} and branch {self.branch}")
+    
+# de=update("Yeshu","Btech",7.2,"CSE")
+# de.update_call()
+
+class student:
+    def __init__(self,Name,regno,branch):
+        self.Name=Name
+        self.regno=regno
+        self.branch=branch
+    
+class mentor(student):
+    def __init__(self,Name,regno,branch,mentorname):
+        super().__init__(Name,regno,branch)
+        self.mentorname=mentorname
+    
+    def mentor_call(self):
+        print(f"Name of the student {self.Name} regno number {self.regno} branch {self.branch} mentor name {self.mentorname} ")
+
+update=mentor("Yeshu",12312308,"CSE","Tharun")
+update.mentor_call()
