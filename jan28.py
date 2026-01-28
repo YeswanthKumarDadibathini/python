256ff63a6e6207e53f270d3e84433805cd8e1924
YeswanthKumarDadibathini <dyeswanth1005@gmail.com>
2026-01-28 12:31:15 +0530

    Finding the biggest number using lambda function

---
+++ jan28.py
@@ -0,0 +1,18 @@
+#lambda functions
+
+# def sq(x):
+#     return x**2
+# print(sq(5))
+
+# y=lambda x:x**2
+# print(y(5))
+
+# x=10
+# y=20
+# z=lambda a:(x+y)
+# print(z(0))
+
+a=int(input("a: "))
+b=int(input("b: "))
+c=lambda a,b:a if(a>b) else b
+print(c(a,b))
