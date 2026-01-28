6fe886b4a5dcdccbb6062021e53d9683c960a22f
YeswanthKumarDadibathini <dyeswanth1005@gmail.com>
2026-01-28 12:43:09 +0530

    using map function in lambda function

--- jan28.py
+++ jan28.py
@@ -12,7 +12,11 @@
 # z=lambda a:(x+y)
 # print(z(0))
 
-a=int(input("a: "))
-b=int(input("b: "))
-c=lambda a,b:a if(a>b) else b
-print(c(a,b))
+# a=int(input("a: "))
+# b=int(input("b: "))
+# c=lambda a,b:a if(a>b) else b
+# print(c(a,b))
+
+# list1=[1,2,3,4,5,6]
+# z=list(map(lambda x:x**2,list1))
+# print(z)
